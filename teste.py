08e358aa7a2466f593bbcf1a119b2455b8f91864
Werewolf-moon <isaacpontresdefarias@gmail.com>
2025-09-17 08:53:14 -0300

    pronpo

--- teste.py
+++ teste.py
@@ -4,5 +4,5 @@ def whatever():
 
 whatever();
 
-  git config --global user.email "isaacpontresdefarias@gmail.com"
-  git config --global user.name "Werewolf-moon"
+git config --global user.email "isaacpontresdefarias@gmail.com"
+git config --global user.name "Werewolf-moon"
