84808c9c2a2fb58d1c4e45486dc5e506fca774f2
Werewolf-moon <isaacpontresdefarias@gmail.com>
2025-10-02 18:35:57 -0300

    Modificação dia (30/09/2025) funcionando.

--- Moto.py
+++ Moto.py
@@ -1,27 +1,27 @@
 class Moto:
- def __init__ (self, marca, modelo, ano, cor, velocidade):
-    self.marca= marca
-    self.modelo= modelo
-    self.ano= ano 
-    self.cor= cor 
-    self.velocidade= velocidade
+    def __init__(self, marca, modelo, ano, cor, velocidade):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        self.velocidade = velocidade
 
-def acelerar(self, valor):
- self.velocidade += valor
- print(f"{self.modelo} acelerou para {self.velocidade} KM/H.")
+    def acelerar(self, valor):
+        self.velocidade += valor
+        print(f"{self.modelo} acelerou para {self.velocidade} KM/H.")
 
-def frear(self, valor):
- self.velocidade -= valor
- print(f"{self.modelo} freiou para {self.velocidade} KM/H.")
+    def frear(self, valor):
+        self.velocidade -= valor
+        print(f"{self.modelo} freiou para {self.velocidade} KM/H.")
 
-def detalhes(self):
-  print(f"A marca da moto é {self.marca} O modelo é {self.modelo} O ano de fabricação {self.ano} A cor veiculo é {self.cor} sua velocidade é {self.velocidade}.")
+    def detalhes(self):
+        print(f"A marca da moto é {self.marca}. \nO modelo é {self.modelo}. \nO ano de fabricação é {self.ano}. "
+              f"A cor do veículo é {self.cor}. \nSua velocidade é {self.velocidade} KM/H.")
 
-moto1= Moto("Kawasaki", "ninja", 1770, "azul",56)
-moto2= Moto("Kawajima", "deck", 1556, "vermelho",90)
+moto1 = Moto("Kawasaki", "ninja", 1770, "azul", 56)
+moto2 = Moto("Kawajima", "deck", 1556, "vermelho", 90)
 
-if (moto1.velocidade > moto2.velocidade):
-    print(moto1.detalhes())
+if moto1.velocidade > moto2.velocidade:
+    moto1.detalhes()
 else:
-    print(moto2.detalhes())
-
+    moto2.detalhes()
