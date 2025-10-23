e1955d61170bdff85d55d13612dd490ee16db73d
Werewolf-moon <isaacpontresdefarias@gmail.com>
2025-10-23 20:56:56 -0300

    Atualizado e funcionado

--- Herança2.py
+++ Herança2.py
@@ -1,63 +1,54 @@
 class Pessoa:
-    def __init__(self= None, nome= None, cpf= None, disciplina= None):
+    def __init__(self, nome=None, cpf=None):
         self.nome = nome
         self.cpf = cpf
-        self.disciplina= disciplina
 
-        def apresentar(self):
-            base= super().apresentar()
-            base= apresentar
-            return f"Seu nome é {self.nome}, sua matricula é {self.matricula}, seu cpf é {self.cpf},"
-        
-        if nome is None:
-            nome= input("\n2Qual é seu nome? ")
-        if matricula is None:
-            matricula= input("\n2Digite sua matricula: ")
-        if cpf is None:
-            cpf= input("\n2Digite seu CPF: ")
+    def apresentar(self) -> str:
+        return f"Olá, eu sou o {self.nome} e meu CPF é {self.cpf}."
 
 
 class Aluno(Pessoa):
-    def __init__(self, nome= None, matricula= None, cpf= None):
-        if nome is None:
-            nome= input("\n1Qual é seu nome? ")
-        if matricula is None:
-            matricula= input("\n1Digite sua matricula: ")
-        if cpf is None:
-            cpf= input("\n1Digite seu CPF: ")
-   
+    def __init__(self, nome=None, matricula=None, cpf=None):
         super().__init__(nome, cpf)
         self.matricula = matricula
 
-    def apresentar(self):
-        base= super().apresentar() 
-        return f"{base}, eu sou o aluno da EEP Onelio porto, minha matrícula é {self.matricula}."
+    def apresentar(self) -> str:
+        base = super().apresentar()
+        return f"{base} \nEu sou o aluno da EEP Onelio porto, minha matrícula é {self.matricula}."
 
 class Professor(Aluno):
-    def __init__(self, nome= None, cpf= None, matricula= None, disciplina= None):
-        if nome is None:
-            nome= input("\nDigite o nome de professor: ")
-        if matricula is None:
-            matricula= input("\nDigite sua matricula: ")
-        if cpf is None:
-            cpf= input("\nDigite seu CPF: ")
-        if disciplina is None:
-            disciplina= input("\nDigite a sua disciplina como professor: ")
+    def __init__(self, nome=None, matricula=None, cpf=None, disciplina=None):
+        super().__init__(nome, matricula, cpf)
+        self.disciplina = disciplina
 
-        super().__init__(nome, cpf)
-        super().__init__(matricula)
-        super().__init__(disciplina)
-        self.matricula = matricula
+    def apresentar(self) -> str:
+        return f"Olá, eu sou o {self.nome} e meu CPF é {self.cpf}.\nEu sou o professor da EEP Onelio porto e minha matrícula é {self.matricula}."
 
-    def apresentar(self):
-        base= super().apresentar() 
-        return f"{base}, eu sou o professor da EEP Onelio porto, e minha matricula é {self.matricula}." 
-        
-# Progama principal
-p= Pessoa()  
-a= Aluno()
-prof= Professor()
+# Progama principal 
+print("Nome da pessoa:")
+nome_pessoa = input()
+print("CPF da pessoa:")
+cpf_pessoa = input()
+pessoa = Pessoa(nome_pessoa, cpf_pessoa)
 
-print(p.apresentar())
-print(a.apresentar())
-print(prof.apresentar())
+print("Nome do aluno:")
+nome_aluno = input()
+print("Matrícula do aluno:")
+matricula_aluno = input()
+print("CPF do aluno:")
+cpf_aluno = input()
+aluno = Aluno(nome_aluno, matricula_aluno, cpf_aluno)
+
+print("Nome do professor:")
+nome_professor = input()
+print("Matrícula do professor:")
+matricula_professor = input()
+print("CPF do professor:")
+cpf_professor = input()
+print("Disciplina do professor:")
+disciplina_professor = input()
+professor = Professor(nome_professor, matricula_professor, cpf_professor, disciplina_professor)
+
+print(pessoa.apresentar())
+print(aluno.apresentar())
+print(professor.apresentar())
