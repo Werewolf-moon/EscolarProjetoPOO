4a5558fcc3f9ea9c1293721f3ff55297bf2cd4bd
seduc 09 <Isaac>
2025-10-23 08:17:51 -0300

    melhorias 2

--- antigo/cadastro.py
+++
@@ -1,8 +0,0 @@
-def carro():
-    modelo = input("seu modelo de carro é: ")
-    ano_fab= input("seu ano de fabricação: ")
-    cor = input("digite a cor do carro:")
-
-    print("\nseu modelo", modelo)
-    print("\nseu ano", ano_fab)
-    
